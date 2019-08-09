3399f10ad35328dcb00dc41f15b3cfba020b8d2b
Andre Coelho <andre.ribeiro@iv2.com.br>
2019-08-09 16:39:08 -0300

    ajust babel root import, create page dashboard and component header

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Body } from './styles';
+
+import Header from '~/components/Header';
+
+function Dashboard() {
+  return (
+    <Container>
+      <Header />
+      <Body />
+    </Container>
+  );
+}
+
+export default Dashboard;
